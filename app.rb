7f6835f93b757cc4c2f40f181cfff35606e609d6
sycalvind <114632354+sycalvind@users.noreply.github.com>
2025-02-01 16:31:13 +0000

    complete

--- app.rb
+++ app.rb
@@ -5,6 +5,18 @@ get("/") do
   erb(:homepage)
 end
 
+get("/dice/:number_of_dice/:how_many_sides") do
+  @num_dice = params.fetch("number_of_dice").to_i
+  @sides = params.fetch("how_many_sides").to_i
+  @rolls = []
+  @num_dice.times do
+    die = rand(1..@sides)
+    @rolls.push(die)
+  end
+  erb(:flexible)
+end
+
+=begin
 get("/dice/2/6") do
   @rolls = []
 
@@ -52,3 +64,4 @@ get("/dice/5/4") do
 
   erb(:five_four)
 end
+=end
